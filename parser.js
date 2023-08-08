bb0f39e75c37bd20692dea24a65fc9e95ddef59e
xiaoxin <2053065908@qq.com>
2023-08-08 17:29:35 +0800

    修正某些数据类型的周次识别错误问题

--- parser.js
+++ parser.js
@@ -1,97 +1,87 @@
 function scheduleHtmlParser(outerIframeContent) {
 
-    outerIframeContent = $('table .fc-content-skeleton table')
-
-    let td = $(outerIframeContent).find('td')
+    let td = $('table .fc-content-skeleton table td')
 
     let courseTable = []
 
     for (let i = 1; i < td.length; i++) {
-        let weekCourse = $(td[i]);
-
-        let a = weekCourse.find('a');
-
-        // 获取A标签
+        let a = $(td[i]).find('a')
         for (let j = 0; j < a.length; j++) {
-            let courseObj = {};
+            let aText = $($(td[i]).find('a')[j]).attr('lay-tips')
+            let day = Select(aText, '星期')
+            let name = (Select(aText, '课程')).split(']')[1]
+            let position = Select(aText, '教学场地')
+            let sections = Sections(Select(aText, '节次'))
+            let teacher = Select(aText, '授课教师')
+            let weeks = Week(Select(aText, '上课周次'))
 
-            let div = $(a[j]).find('.fc-title');
+            let courseObj = {}
 
-            let courseInfo = div.find('div');
-
-            //处理周次
-            let weeks = courseInfo.eq(2).text().split('[')[1].split(']')[0];
-            let regex0 = /.-./;
-            let regex1 = /.,./;
-            let weeksArr = [];
-            // console.log("%c" + typeof (weeksArr), "color:red")
-
-            if (weeks.match(regex0) && !weeks.match(regex1)) {
-                let f = Number(weeks.split('-')[0]);
-                let s = Number(weeks.split('-')[1].split('周')[0]);
-
-                if (f < s) {
-                    for (let i = f; i <= s; i++) {
-                        weeksArr.push(i);
-                    }
-                }
-            }
-
-            if (!weeks.match(regex0) && weeks.match(regex1)) {
-                weeksArr = weeks.split('周')[0].split(',').map(Number);
-            }
-
-            if (!weeks.match(regex0) && !weeks.match(regex1)) {
-                let weeksNumber = Number(weeks.split('周')[0]);
-                weeksArr.push(weeksNumber);
-            }
-
-            if (weeks.match(regex0) && weeks.match(regex1)) {
-                weeks = weeks.split('周')[0];
-                let f = weeks.split(',')[0];
-                let s = weeks.split(',')[1];
-
-                let f1 = f.split('-')[0];
-                let s1 = f.split('-')[1];
-
-                let f2 = s.split('-')[0];
-                let s2 = s.split('-')[1];
-
-                if (f1 < s1) {
-                    for (let i = f1; i <= s1; i++) {
-                        weeksArr.push(i);
-                    }
-                }
-
-                if (f2 < s2) {
-                    for (let i = f2; i <= s2; i++) {
-                        weeksArr.push(i);
-                    }
-                }
-            }
-
-            // 处理节次 01-02节
-            let sectionsArr = [];
-            let sections = courseInfo.eq(0).text().split('节')[0];
-            let f = Number(sections.split("-")[0]);
-            let s = Number(sections.split('-')[1]);
-
-            if (f < s) {
-                for (let i = f; i <= s; i++) {
-                    sectionsArr.push(i);
-                }
-            }
-
-            courseObj.name = courseInfo.eq(1).text();
-            courseObj.position = courseInfo.eq(3).text();
-            courseObj.teacher = courseInfo.eq(2).text().split('[')[0];
-            courseObj.weeks = weeksArr.map(Number);
-            courseObj.day = i;
-            courseObj.sections = sectionsArr.map(Number);
+            courseObj.name = name
+            courseObj.position = position
+            courseObj.teacher = teacher
+            courseObj.weeks = weeks
+            courseObj.day = Number(day)
+            courseObj.sections = sections
 
             courseTable.push(courseObj);
+
         }
     }
-
     return courseTable;
 }
+
+
+function Select(aText, str) {
+    return $(aText).find('th:contains("' + str + '")').next('td').text()
+}
+
+
+// 处理节次 第01-02节 返回数字类型数组
+function Sections(str) {
+    let matches = str.match(/(\d+)-(\d+)/);
+
+    if (matches && matches.length >= 3) {
+        let start = parseInt(matches[1]);
+        let end = parseInt(matches[2]);
+        let result = [];
+        for (let i = start; i <= end; i++) {
+            result.push(i);
+        }
+
+        return result
+    }
+}
+
+
+
+// 处理周次
+function Week(str) {
+
+    // str = '7,7-8,8-9,9-10,10-16,30-25,1,2,4'
+    str = str.split(',')
+    strArr = []
+    for (let i = 0; i < str.length; i++) {
+        if ((str[i]).includes('-')) {
+            let temp = (str[i].split('-')).map(Number)
+            if (temp[0] <= temp[1]) {
+                for (let j = temp[0]; j <= temp[1]; j++) {
+                    strArr.push(j)
+                }
+            }else{
+                for (let j = temp[1]; j <= temp[0]; j++) {
+                    strArr.push(j)
+                }
+            }
+        } else {
+            strArr.push(Number(str[i]))
+        }
+    }
+    // console.log(strArr)
+    // console.log(Array.from(new Set(strArr)))
+
+
+
+    return Array.from(new Set(strArr))
+}
+
